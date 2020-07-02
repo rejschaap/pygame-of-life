99adc96ffa3d753639624260b1efcb5ed751f673
Robert Schaap <rejschaap@gmail.com>
2020-07-02 10:50:03 +0200

    Add some controls

--- main.py
+++ main.py
@@ -16,28 +16,45 @@ GLIDER = [(0, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
 
 
 def main():
+    state = dict(pause=False, selecting=False, clear=False, init=False, gliders=0)
     clock = pygame.time.Clock()
     screen = create_screen()
-    board = init_with_gliders(10)
+    board = add_gliders([], 10)
 
     while True:
-        handle_events()
-        board = update(board)
-        draw(screen, board)
+        state = handle_events(state)
+
+        if state["gliders"]:
+            board = add_gliders(board, state["gliders"])
+
+        if state["clear"]:
+            board = []
+
+        if not state["pause"] and not state["selecting"]:
+            board = update(board)
+        elif "step" in state:
+            board = update(board)
+
+        if "select" in state:
+            for pos in state["select"]:
+                if pos not in board:
+                    board.append(pos)
+
+        draw(screen, board, state["pause"])
         pygame.display.update()
         clock.tick(FRAME_RATE)
 
 
-def init_with_gliders(count):
-    """Place the specified amount of gliders on the board"""
-    board = []
+def add_gliders(board, count):
+    new_board = board.copy()
 
+    """Place the specified amount of gliders on the board"""
     for i in range(count):
         dx, dy = int(random() * WIDTH), int(random() * HEIGHT)
         translated_glider = [(x + dx, y + dy) for (x, y) in GLIDER]
-        board.extend(translated_glider)
+        new_board.extend(translated_glider)
 
-    return board
+    return new_board
 
 
 def update(board):
@@ -70,11 +87,11 @@ def count_neighbours(state, x, y):
     return count
 
 
-def draw(screen, board):
+def draw(screen, board, paused):
     """Draw the board on the screen"""
     draw_fade(screen, 0.25)
     draw_live_cells(screen, board)
-    draw_grid(screen)
+    draw_grid(screen, paused)
 
 
 def draw_fade(screen, percentage):
@@ -92,20 +109,43 @@ def draw_live_cells(screen, board):
         pygame.draw.rect(screen, FRONT_COLOR, rect, 0)
 
 
-def draw_grid(screen):
+def draw_grid(screen, paused):
     """Draw grid lines on top of everything"""
+    color = GRID_COLOR if paused else BACK_COLOR
     for y in range(HEIGHT):
         for x in range(WIDTH):
             rect = pygame.Rect(x * ZOOM, y * ZOOM, ZOOM, ZOOM)
-            pygame.draw.rect(screen, GRID_COLOR, rect, 1)
+            pygame.draw.rect(screen, color, rect, 1)
 
 
-def handle_events():
+def handle_events(state):
     """Quit on keypress"""
+    new_state = dict(pause=state["pause"], selecting=state["selecting"], select=[], clear=False, gliders=0)
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            new_state["pause"] = not state["pause"]
+        elif event.type == pygame.KEYDOWN and event.key >= pygame.K_0 and event.key <= pygame.K_9:
+            new_state["gliders"] = event.key - pygame.K_0
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE:
+            new_state["clear"] = True
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_n:
+            new_state["step"] = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            new_state["selecting"] = True
+            pos = (event.pos[0] // ZOOM, event.pos[1] // ZOOM)
+            new_state["select"].append(pos)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            new_state["selecting"] = False
+        elif event.type == pygame.MOUSEMOTION and new_state["selecting"]:
+            pos = (event.pos[0] // ZOOM, event.pos[1] // ZOOM)
+            if pos not in new_state["select"]:
+                new_state["select"].append(pos)
+        elif event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and (event.key == pygame.K_q or event.key == pygame.K_ESCAPE)):
             pygame.quit()
             sys.exit()
+
+    return new_state
 
 
 def create_screen():
